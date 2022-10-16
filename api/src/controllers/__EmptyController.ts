5acc2abb87cf206670c8b13b9e01e9ef967d798a
vincentDubresson <vincent.dubresson@orange.fr>
2022-10-16 18:05:30 +0200

    Creation of an empty controller

---
+++ api/src/controllers/__EmptyController.ts
@@ -0,0 +1,19 @@
+export const get = async (req: Request, res: Response): Promise<void> => {
+
+}
+
+export const getById = async (req: Request, res: Response): Promise<void> => {
+  
+}
+
+export const post = async (req: Request, res: Response): Promise<void> => {
+  
+}
+
+export const put = async (req: Request, res: Response): Promise<void> => {
+  
+}
+
+export const del = async (req: Request, res: Response): Promise<void> => {
+  
+}
